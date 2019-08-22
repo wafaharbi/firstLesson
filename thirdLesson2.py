3475e565a68bab6f8dc8bef6b987705ff6b8eddd
wafaharbi <43577974+wafaharbi@users.noreply.github.com>
2019-08-22 20:55:08 +0300

    Add files via upload
    second part of lesson three

---
+++ thirdLesson2.py
@@ -0,0 +1,22 @@
+# fifth challenge on 3thd date 
+n = m = p = "Orange"
+print(n)
+print(m)
+print(p)
+
+
+# sisxth challenge on 3thd date 
+w = "awesome"
+print(" Python is " + w)
+
+# seventh challenge on 3thd date 
+e = "Python is"
+u ="awesome"
+o = e+u
+print(o)
+
+# eight challenge on 3thd date 
+l = 5
+g =10
+print(l+g)
+
