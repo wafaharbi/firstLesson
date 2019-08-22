4609407d90ab754485556a78e47026af210bf8d6
wafaharbi <43577974+wafaharbi@users.noreply.github.com>
2019-08-22 20:51:01 +0300

    Add files via upload
    thirdLessonPartOne

---
+++ thirdLesson.py
@@ -0,0 +1,25 @@
+
+# first challenge on 3thd date 
+x = 5
+y = "John"
+print(x)
+print(y)
+
+# second challenge on 3thd date 
+x = 4 # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+# third challenge on 3thd date 
+x = "John"
+# is same as
+x ='John'
+
+# fourth challenge on 3thd date 
+s,t,r= "orange", " Banana" , "Cherry"
+print(s)
+print(t)
+print(r)
+
+
+
