309111a49c600903eb18a8c7d93389705ad1f2ef
wafaharbi <43577974+wafaharbi@users.noreply.github.com>
2019-08-23 20:10:26 +0300

    second Day

---
+++ secondLesson.py
@@ -0,0 +1,10 @@
+
+# __________ second Challenges in 100 Programming code ____________#
+
+print("Hello World!") # This is a comment
+
+"""This is a comment
+  Written in
+ more then just one line"""
+
+print("Cheers , Mate!")
