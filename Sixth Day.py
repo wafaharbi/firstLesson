17fe0e5b7c28d8eb8435c8a09392ed74c8702981
wafaharbi <43577974+wafaharbi@users.noreply.github.com>
2019-08-25 11:16:58 +0300

    Sixth Day

---
+++ Sixth Day.py
@@ -0,0 +1,44 @@
+#  _______________ Sixth Day _________________ #
+
+
+
+# To convert an integer number
+
+x = int(1)      # x will be 1
+y = int(2.8)    # y will be 2
+z = int("3")    # z will be 3
+
+print("Convert 1 to integer",x)        # print 1   
+print("Convert 2.8 to integer" ,y)     # print 2
+print("Convert 3 to integer",z)        # print 3
+
+print("\n")
+
+# To convert a float number
+
+x = float(1)      # x will be 1.0
+y = float(2.8)    # y will be 2.8
+z = float("3")    # z will be 3.0
+w = float("4.2")  # w will be 4.2
+
+print("Convert 1 to float",x)          # print 1
+print("Convert 2.8 to float", y)       # print 2.8
+print("Convert 3 to float", z)         # print 3
+print("Convert 4.2 to float", w)       # print 4.2
+
+print("\n")
+
+
+# To convert a String
+
+x = str("S1")     # x will be 'S1'
+y = str(2)        # y will be '2' 
+z = str(3.0)      # z will be '3.0'
+
+print("Convert S1 to string" , x)
+print("Convert 2 to string" , y)
+print("Convert 3.0 to string" , z)
+
+print("\n")
+
+
