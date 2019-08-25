4c57daccdb4df5bf27ab90c8f256538cc602236c
wafaharbi <43577974+wafaharbi@users.noreply.github.com>
2019-08-25 10:05:36 +0300

    updated to print seperated word

--- fifthDay.py
+++ fifthDay.py
@@ -21,3 +21,13 @@ n = 2    # number of splitting characters
 basket=[basket[i:i+n] for i in range(0 , len(basket) , n)]
 
 print(basket)    #That print ['ap', 'pl', 'eo', 'ra', 'ng', 'el', 'im', 'on']
+
+
+print("\n")
+
+
+basket = [x , y, z]
+n = [5]
+
+basket = [basket[sum(n[:i]):sum(n[:i+1])] for i in range(len(n))]
+print("The word in seperated word", basket)        #print apple orange limon
