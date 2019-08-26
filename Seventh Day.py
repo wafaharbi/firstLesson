ccbc0d39d2e6848881c35dfc8d25b1e34b349ac2
wafaharbi <43577974+wafaharbi@users.noreply.github.com>
2019-08-26 18:58:43 +0300

    Seventh Day

---
+++ Seventh Day.py
@@ -0,0 +1,32 @@
+# ________________ Seventh Day ________________ #
+
+""" The String in Python
+  'hello' is the same as "hello"""
+
+print("Hello")
+print('Hello')
+
+print("\n")
+
+# Assigning a string to a variable
+a = "Hello"
+print(a , "\n")
+
+
+multi = """ You can assign a multiline string
+to a variable by using three quotes."""
+
+print(multi)
+print("\n")
+
+# Srings using Arrays
+a = "Hello"
+print("Print the second letter: e =>",a[1])
+
+print("\n")
+
+#In the String characters from position 2 to position 5
+b = "Hello, World!"
+print(b[2:5])
+
+print("\n")
