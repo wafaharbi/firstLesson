5cc101bb40b21ef9ab4a2a405cae792a2c50a523
wafaharbi <43577974+wafaharbi@users.noreply.github.com>
2019-08-27 10:40:25 +0300

    Eighth Day

---
+++ Eighth Day.py
@@ -0,0 +1,41 @@
+# ____________ Eighth Day _______________ #
+
+a = " Hello, World! "
+
+print(a.strip())      # print Hello, World!
+
+print("\n")
+
+
+
+# The len method return length of string
+
+a = "Hello, World!"
+
+print("The length of string :",len(a))        # print the length of the string
+print("\n")
+
+
+# The lower method return the string in lower case
+
+a = "Hello, World!"
+print("The string in lower case ",a.lower(),"\n")      # print the lower case
+
+
+# The upper method return the string in upper case
+
+a = "Hello, World!"
+print("The string in upper case ",a.upper(),"\n")      # print the upper case
+
+
+# The upper method return the string in upper case
+
+a = "Hello, World!"
+print(a.replace("H", "J"), "\n")                            # replace the h to j
+
+
+# The split method use to split the string
+
+a = "Hello, World!"
+print(a.split(","))                                     # return ['Hello' , 'World!']
+
