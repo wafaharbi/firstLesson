655990e2785b778bb758b806c728d9b8807e6631
wafaharbi <43577974+wafaharbi@users.noreply.github.com>
2019-08-29 10:31:00 +0300

    Tenth Day

---
+++ Tenth Day.py
@@ -0,0 +1,28 @@
+# ________________ Tenth Day _________________ #
+
+# Arithmetic Operators in Python
+
+x = 5
+y = 3
+
+print("  Arithmetic Operators:  ")
+print("The addition for 5 and 3 is ",x + y,"\n")
+print("The substraction for 5 and 3 is ",x - y,"\n")
+print("The multiplication for 5 and 3 is ",x * y,"\n")
+print("The Division for 5 and 3 is ",x / y,"\n")
+
+
+# Assignment Operators in Python
+
+x = 5
+x /= 3
+print("Assignment Operators of 5 is ",x , "\n")
+
+# Comparison Operators in Python
+
+x = 5
+y = 3
+
+# this will return false because 5 is larger than 3
+print("The Comparison Operators of 5 < 3 is ",x < y)
+
