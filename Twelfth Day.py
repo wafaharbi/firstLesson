7877041d535bb22158a859524de321579d9788fa
wafaharbi <43577974+wafaharbi@users.noreply.github.com>
2019-08-30 23:23:41 +0300

    Twelfth Day

---
+++ Twelfth Day.py
@@ -0,0 +1,23 @@
+# _________________ Twelfth Day ___________________ #
+
+
+# 1-  Replace I Pronouns to We Pronouns by use replace() function
+
+""" 2 -  In empty brackets, using using format() function,
+fill the first space with the number 5 and the second
+space with the number"""
+
+
+# 3 - Convert every "a" letters to capital by use uppercase() function
+
+
+sentance = "Please , I want {} sandwishes and  {} donutes"
+
+x = 5
+y = 7
+
+print(sentance.replace(" I ", " We ").format(x,y).upper())
+
+
+
+
