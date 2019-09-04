12d7a7b280e8614a53aee8b95f0ca7c53e1b0e0f
wafaharbi <43577974+wafaharbi@users.noreply.github.com>
2019-09-04 11:27:25 +0300

    Fourteenth Day

---
+++ FourTeenth Day.py
@@ -0,0 +1,20 @@
+#  _______________ FourTeenth _________________ #
+
+# Python List2
+
+lists = ["A","B","C","D","E"]
+print(lists[1:3])
+
+lists = ["ahmed","omar","sami"]
+if "ahmed" in lists:
+    print("yes, 'ahmed' is in the list name ")
+    
+lists = ["python"]*4
+print(lists)
+
+lists1 = ["ahmed","omar","sami"]
+lists2 = ["wafa","daliah","afnan"]
+
+list3= lists1 + lists2
+
+print(list3)
