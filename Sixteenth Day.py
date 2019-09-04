7f4b9bf0c648e0be27eeb7b291dd3e2a9421e695
wafaharbi <43577974+wafaharbi@users.noreply.github.com>
2019-09-04 11:40:05 +0300

    Sixteenth Day

---
+++ Sixteenth Day.py
@@ -0,0 +1,46 @@
+# ____________________ Sixteenth Day _________________ #
+
+# Python Tuples
+
+thisTuple = ("Noura","Amnah","Sara")
+print(thisTuple)
+
+#Empty tuples 
+thisTuple = ()
+print(thisTuple)
+
+
+thisTuple = (3,)
+print(thisTuple)
+
+
+thisTuple = (3, 1.3 ,4.2 , 7)
+print(thisTuple)
+
+
+thisTuple = ("Daliah", 1.1 , 4, "Python")
+print(thisTuple)
+
+
+thisTuple = ("Noura","Amnah","Sara")
+
+print(thisTuple[1])
+
+thisTuple = ("Noura","Amnah","Sara")
+for x in thisTuple:
+    print(x)
+
+
+
+thisTuple = ("Noura","Amnah","Sara")
+#thisTuple[3] = "wafa"          this will print an error
+print(thisTuple)
+
+
+thisTuple = ("Noura","Amnah","Sara")
+#del thisTuple                  this will print an error
+print(thisTuple)
+
+
+thisTuple = ("Noura","Amnah","Sara")
+print(thisTuple[0:3])
