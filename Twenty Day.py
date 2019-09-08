01a7422e912d04384dc0e1b0eedbaef64b995f8b
wafaharbi <43577974+wafaharbi@users.noreply.github.com>
2019-09-08 10:54:50 +0300

    Twenty Day

---
+++ Twenty Day.py
@@ -0,0 +1,36 @@
+# __________________ Twenty Day ________________ #
+
+# Python Sets
+
+sets = {}
+print(sets)
+
+
+
+# create a set
+sets = {"wafa","fatima","noura"}
+print(sets)
+
+
+sets = {"wafa","fatima","noura",1,2,3}
+print(sets)
+
+
+sets = {"wafa","fatima","noura"}
+for x in sets:
+    print(x)
+
+sets = {"wafa","fatima","noura"}
+print("wafa" in sets)
+
+
+sets = {"wafa","fatima","noura"}
+sets.add("sara")
+print(sets)
+
+
+sets = {"wafa","fatima","noura"}
+sets.update(["sara","maha","hanan"])
+print(sets)
+
+
