a8109aa2849a7d6cc7b78a998e8647d6be16cc6e
wafaharbi <43577974+wafaharbi@users.noreply.github.com>
2019-09-17 18:05:55 +0300

    28 day

---
+++ 28 Day.py
@@ -0,0 +1,51 @@
+# ___________________ 28 Day _____________ #
+
+# Python Loop
+
+
+
+
+print("\n"," while loop statement")
+
+# While loop
+
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+
+print("\n"," break statement")
+
+
+# break statement
+i = 1
+while i < 6:
+    print(i)
+    if i == 3:
+        break
+    i += 1
+
+print("\n"," continue statement")
+   
+# continue statement
+i = 0
+while i < 6:
+    i += 1
+    if i == 3:
+        continue
+    print(i)
+
+    
+print("\n"," esle statement")
+
+# else statement
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+else:
+    print(" i is no longer less than 6")
+    
+  
+
+        
