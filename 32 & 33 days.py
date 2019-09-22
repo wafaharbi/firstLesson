d3703816da8dbcb15ab4f48e650244c9f84fd7b1
wafaharbi <43577974+wafaharbi@users.noreply.github.com>
2019-09-22 15:12:55 +0300

    32 & 33 Days

---
+++ 32 & 33 days.py
@@ -0,0 +1,16 @@
+# _________________ 32 and 33 Days _________________ #
+
+
+
+# print a list A from 3 to 17
+
+# print a list B from 2 to 16
+
+ListA = [3, 5, 7, 9 ,11, 13, 15, 17]
+ListB = [2, 4, 6, 8, 10, 12, 14, 16]
+
+for x in range(3, 18, 2):
+    for y in range(2, 17, 2):
+        print(x , " => ",y)
+
+        
