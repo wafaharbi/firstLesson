dc143a7d671568ab8f5e8ea0c03ee7d3185f7c8a
wafaharbi <43577974+wafaharbi@users.noreply.github.com>
2019-09-24 10:42:40 +0300

    35 Day

---
+++ 35 Day.py
@@ -0,0 +1,15 @@
+# ________________ 35 Day _______________ #
+
+#Python Lambda function
+
+x = lambda a : a + 10
+print(x(5))
+
+
+# lambda function take any number of arguments
+x = lambda a, b : a * b
+print(x(5, 8))
+
+
+x = lambda a ,b ,c : a + b + c
+print(x(2,3,4))
