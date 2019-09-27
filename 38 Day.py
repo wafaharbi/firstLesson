d60973b3a63366b7a3bc19464e05f6657e41097e
wafaharbi <43577974+wafaharbi@users.noreply.github.com>
2019-09-27 14:17:24 +0300

    38 Day

---
+++ 38 Day.py
@@ -0,0 +1,29 @@
+# ________________ 38 Day ___________________ #
+
+# Python Array2
+
+#loop array elements
+
+names = ["wafa","afnan","noura"]
+for x in names:
+    print(x)
+    
+#adding array elements
+
+names = ["wafa","afnan","noura"]
+names.append("Aish")
+print(names)
+
+
+#removing array elements
+
+names = ["wafa","afnan","noura"]
+names.pop(1)
+print(names)
+
+
+# using remove() method
+
+names = ["wafa","afnan","noura"]
+names.remove("afnan")
+print(names)
