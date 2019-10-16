4dd9ab45123c695d58347fa787ab0df54b415c34
wafaharbi <43577974+wafaharbi@users.noreply.github.com>
2019-10-16 19:12:06 +0300

    52 Days

---
+++ 52 Day.py
@@ -0,0 +1,28 @@
+# ________________ 52 _____________ #
+
+# Python datetime
+
+#Python Date
+
+
+import datetime
+x = datetime.datetime.now()
+print(x)
+
+
+import datetime
+x = datetime.datetime.now()
+
+print(x.year)
+print(x.strftime("%A"))
+
+
+import datetime
+x = datetime.datetime(2020, 5, 17)
+print(x)
+
+
+import datetime
+
+x = datetime.datetime(2019, 6, 1)
+print(x.strftime("%B"))
