e084cbbe70a551a1844a7c1853547f2dc374d9b4
wafaharbi <43577974+wafaharbi@users.noreply.github.com>
2019-11-07 19:34:09 +0300

    62 day

---
+++ SixtyTwoDay.py
@@ -0,0 +1,16 @@
+# ________________ 62 day __________________ #
+
+# Python PIP
+
+
+
+import camelcase
+
+c = camelcase.CamelCase()
+
+txt = "loren ipsum dolor sit amet"
+print(c.hump(txt))
+
+
+#This method for Capitalzes the first letter of each word
+
