3090a1053379bce2e9c73466adfea93658e1eaee
wafaharbi <43577974+wafaharbi@users.noreply.github.com>
2019-11-08 11:49:53 +0300

    67 & 68 Days

---
+++ 67 & 68 Days  .py
@@ -0,0 +1,22 @@
+# ______________ 67 & 68 Days _____________ #
+
+#Challenge one
+
+
+print("enter your first latter in yourname:")
+Fname = input()
+
+print("enter your last letter in your name:")
+Lname = input()
+
+print("Your name is statr with", Fname,"and end with", Lname)
+
+
+#Challenge two
+
+name = "Ahmed Ali"
+balance = 53
+sent = "Dear {}, your current balance is {:.2f}$"
+
+print(sent.format(name, balance))
+
